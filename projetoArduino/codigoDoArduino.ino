bfc6205948f3fbedb55f9d6a01888a87a3986e04
ciceroAugustoON <augustocicero831@gmail.com>
2023-06-02 15:33:23 -0300

    Versão 0.2

--- projetoArduino/codigoDoArduino.ino
+++ projetoArduino/codigoDoArduino.ino
@@ -2,7 +2,9 @@ const int analogInPin = A0;         // Pino analógico que o sensor está conect
 const int digitalPin = 2;
 const int analogInPin1 = A1;
 const int acionamento = 0;
+const int sensorPin = 7;
 
+int sensorRead = 0;
 int sensorValue = 0;                     // valor que será lido do sensor
 
 int setpoint = 200;                      // Este valor deverá ser alterado de acordo com a intensidade de
@@ -12,24 +14,26 @@ void setup() {
   Serial.begin(9600);                    // inicializa a comunicação serial com a taxa de 9600 bps
   pinMode(digitalPin, OUTPUT);
   pinMode(analogInPin1,INPUT);
+  pinMode(sensorPin,INPUT);
 }
 
 void loop() {
- 
+  
+  sensorRead = digitalRead(sensorPin);
   sensorValue = analogRead(analogInPin); // Faz a leitura do pino analógico
  
   Serial.print("sensor = " );            // Imprime o resultado no monitor serial
   Serial.println(sensorValue);      
  
-  if(sensorValue < setpoint){
-    digitalWrite(2,HIGH);
+  if(sensorValue < setpoint && sensorRead == HIGH){
+
+    digitalWrite(digitalPin, HIGH); 
+    
   } else {
-    digitalWrite(2,LOW);
-digitalWrite(digitalPin, LOW);
+
+	digitalWrite(digitalPin, LOW);
  
   }
 
   delay(2);                              // Aguarda 2 milisegundos para estabilizar o conversor ADC                  
 }
-
-  
